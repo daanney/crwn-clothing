7e0f8554c69ef16d97e3e6f3a4dab1738d3f316b
danney <danney@macbook.local>
2020-05-01 18:11:40 +0200

    broken homepage into components and finished styling menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './homepage.styles.scss'
+
+import Directory from '../../components/directory/directory.component'
+
+export const HomePage =()=> (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
