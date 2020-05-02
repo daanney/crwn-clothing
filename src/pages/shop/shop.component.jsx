21de7a43dabe7ef072e62875be3ed9c1cb0fa311
danney <danney@macbook.local>
2020-05-02 20:18:31 +0200

    added reducers for shop data and directory

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
 
-import { SHOP_DATA } from './shop.data'
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
+import { selectCollections } from '../../redux/shop/shop.selectors'
+
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-export default class ShopPage extends React.Component {
+const ShopPage =({ collections })=> (
+    <div className='shop-page'>
+        <h1>Shop Page</h1>
+        {collections.map(({id, ...collectionProps}) => (
+            <CollectionPreview key={id} {...collectionProps} />
+        ))}
+    </div>
+)
 
-    constructor(props) {
-        super(props)
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state
-        return (
-            <div className='shop-page'>
-                <h1>Shop Page</h1>
-                {collections.map(({id, ...collectionProps}) => (
-                    <CollectionPreview key={id} {...collectionProps} />
-                ))}
-            </div>
-        )
-    }
-}
+export default connect(mapStateToProps)(ShopPage)
