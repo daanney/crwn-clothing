294fca1c433ba48ec623ff7ddf54a2e7a6379176
danney <danney@macbook.local>
2020-05-03 14:00:08 +0200

    converted cart-dropdown to styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,19 +10,19 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
 
-import './cart-dropdown.styles.scss'
+import { CartDropdownContainer, CartItemsContainer, EmptyMessageContainer } from './cart-dropdown.styles.jsx'
 
 const CartDropdown =({ cartItems, history, dispatch })=> (
-	<div className='cart-dropdown'>
-		<div className='cart-items'>
+	<CartDropdownContainer>
+		<CartItemsContainer>
 			{cartItems.length ? (cartItems.map(item => <CartItem key={item.id} item={item} />)) 
-				: <span className='empty-message'>Your cart is empty</span>}
-		</div>
+				: <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>}
+		</CartItemsContainer>
 		<CustomButton onClick={() => {
 			history.push('/checkout')
 			dispatch(toggleCartHidden())
 		}}>Go to checkout</CustomButton>
-	</div>
+	</CartDropdownContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
