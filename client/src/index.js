3ae4fcfe72f5881436f1c6094de4141e94192d9b
danney <danney@macbook.local>
2020-05-10 23:22:49 +0200

    updating code to be PWA compliant, added service workers, logos and sslify to server

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import './index.css'
 
 import App from './App'
 import { store, persistor } from './redux/store'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -21,3 +22,5 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('root')
 );
+
+serviceWorker.register()
